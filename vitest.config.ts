56dff19b329e0f81c2ac79d1c167e4d65337e328
Ryan Rosello <goneby30+github@gmail.com>
2024-01-14 20:05:35 +1000

    Add lcov reporter for code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,11 @@ import {defineConfig} from 'vitest/config';
 export default defineConfig({
   test: {
     exclude: ['**/node_modules/**', '**/build/**'],
-    reporters: ['html'],
+    // reporters: ['html'],
     coverage: {
       enabled: true,
       provider: 'v8',
-      reporter: ['text', 'html', 'lcov'],
+      reporter: ['text', 'html', 'lcovonly'],
     },
   },
 });
